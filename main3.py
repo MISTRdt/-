030ae599f328b8cbb9f37aaaa24b5e23aad5a043
MISTRdt <164991923+MISTRdt@users.noreply.github.com>
2024-03-26 12:49:05 +0600

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,8 @@
+if '34' > '123':
+    print('yes')
+
+if '123' > '12':
+    print('yes')
+
+if [1, 2] > [1, 1]:
+    print('yes')
---
+++ main4.py
@@ -0,0 +1,8 @@
+if '6' > 5:
+    print('yes')
+
+if [5, 6] > 5:
+    print('yes')
+
+if '6' != 5:
+    print('yes')
